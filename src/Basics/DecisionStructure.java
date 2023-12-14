d60b8084b7557e53f477bf42a8a5c823720e03be
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-12-14 21:21:27 +0530

    Day 1 crap

---
+++ src/Basics/DecisionStructure.java
@@ -0,0 +1,15 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class DecisionStructure {
+    public static void main(String[] args) {
+        double salary = 1000;
+        double bonus = 200;
+        int quota = 10;
+        System.out.println("How many sales did teh employee get this week?");
+        Scanner scanner = new Scanner(System.in);
+        int sales = scanner.nextInt();
+        
+    }
+}
